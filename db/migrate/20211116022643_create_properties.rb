9950e526f92283827b2660e7f876634800fb6acd
Kyle <kylereaves@gmail.com>
2021-11-16 19:25:28 -0500

    Updating property record

--- db/migrate/20211116022643_create_properties.rb
+++ db/migrate/20211116022643_create_properties.rb
@@ -1,6 +1,17 @@
 class CreateProperties < ActiveRecord::Migration[6.1]
   def change
     create_table :properties do |t|
+      t.string :street_address
+      t.string :owner_name
+      t.string :owner_mailing_address
+      t.string :city_state_zip
+      t.string :property_full_address
+      t.int :number_properties_owned
+      t.int :units_at_property
+      t.text :list_properties_owned
+      t.text :g_code
+      t.float :latitude
+      t.float :longitude
 
       t.timestamps
     end
