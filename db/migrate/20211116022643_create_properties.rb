8f4242b0ae700bf7deb90933ed35ccd7089dd7a1
Kyle <kylereaves@gmail.com>
2021-11-20 15:57:28 -0500

    Fixing incorrect field types

--- db/migrate/20211116022643_create_properties.rb
+++ db/migrate/20211116022643_create_properties.rb
@@ -6,8 +6,8 @@ class CreateProperties < ActiveRecord::Migration[6.1]
       t.string :owner_mailing_address
       t.string :city_state_zip
       t.string :property_full_address
-      t.int :number_properties_owned
-      t.int :units_at_property
+      t.integer :number_properties_owned
+      t.integer :units_at_property
       t.text :list_properties_owned
       t.text :g_code
       t.float :latitude
