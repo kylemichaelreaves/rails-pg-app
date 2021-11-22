53d4469157e3f7591a7fb379647749623a928591
Kyle <kylereaves@gmail.com>
2021-11-21 22:44:27 -0500

    Removed column from properties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_012027) do
+ActiveRecord::Schema.define(version: 2021_11_22_034139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_012027) do
     t.string "owner_mailing_address"
     t.string "city_state_zip"
     t.string "property_full_address"
-    t.integer "number_properties_owned"
     t.integer "units_at_property"
     t.text "g_code"
     t.float "latitude"
