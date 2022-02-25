07ecb8623d85b90cc30db419579813ff8ceaf308
Kyle Reaves <89040357+KyleReaves@users.noreply.github.com>
2022-02-25 14:10:05 -0500

    Removed routes/Index…moved it elsewhere
    A few package updates. Generated a few components to be built later.
    Following along with the [react-router
    tutorial](https://reactrouter.com/docs/en/v6/getting-started/tutorial).
    
    Using an useFetch component from
    [useHooks](https://usehooks-ts.com/react-hook/use-fetch)…considering
    other hooks for routing…
    
    added 'react-router-bootstrap

--- app/javascript/Index.jsx
+++ app/javascript/Index.jsx
@@ -2,19 +2,24 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import { BrowserRouter } from 'react-router-dom'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Home from './components/Home'
-import * as bootstrap from 'bootstrap';
+import Properties from './routes/Properties';
+import Landlords from './routes/Landlords';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="api/v1/properties" element={<Properties />} />
+        <Route path="api/v1/landlords" element={<Landlords />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-    document.getElementById('root')
+  document.getElementById('root')
 );
--- app/javascript/components/NavBar.tsx
+++ app/javascript/components/NavBar.tsx
@@ -2,32 +2,21 @@ import * as React from "react";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function NavBar() {
   return (
     <Nav justify variant="tabs" defaultActiveKey="/home">
       <Nav.Item>
-        {/* <Link
-          to="api/v1/properties"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          Properties
-        </Link> */}
-        <Nav.Link eventKey="api/v1/properties">Properties</Nav.Link>
+        <LinkContainer to="api/v1/properties">
+          <Nav.Link>Properties</Nav.Link>
+        </LinkContainer>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="api/v1/landlords">Landlords</Nav.Link>
-        {/* <Link
-          to="/landlords"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          Landlords
-        </Link> */}
+        <Nav.Link as={Link} to="api/v1/landlords">Landlords</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="link-2">Map</Nav.Link>
+        <Nav.Link as={Link} to="link-2">Map</Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link eventKey="disabled" disabled>
