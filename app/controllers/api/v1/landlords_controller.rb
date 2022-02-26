0f074c1e68bd92a6fa3539b7ed3205559d394e27
Kyle <kylereaves@gmail.com>
2022-02-26 14:35:40 -0500

    API Calls working
    The API now returns `Property` and `Landlord` records as JSON objects,
    which will soon be rendered within their respective functional
    components.

--- app/controllers/api/v1/landlords_controller.rb
+++ app/controllers/api/v1/landlords_controller.rb
@@ -1,9 +1,20 @@
 class Api::V1::LandlordsController < ApplicationController
   def index
-    @landlords = Landlord.all
+    landlords = Landlord.all
+    render json: landlords
   end
 
   def show
-      @landlord = Landlord.find(params[:id])
+    if landlord
+      render json: landlord
+    else
+      render json: landlord.errors
+    end
+  end
+
+  private
+
+  def landlord
+    landlord ||= Landlord.find(params[:id])
   end
 end
