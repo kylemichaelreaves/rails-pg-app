a13e1e89d15510ca21553b409651ca0b32d4e21e
Kyle Reaves <89040357+KyleReaves@users.noreply.github.com>
2022-03-01 13:39:12 -0500

    Fixing routes…

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,17 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :properties, :landlords
-      
+
       get 'properties/index', to: 'properties#index'
+      get 'properties/:id', to: 'properties#show'
       post 'properties/create'
-      get 'properties/show/:id', to: 'properties#show'
       delete 'properties/destroy'
       get 'landlords/index', to: 'landlords#index'
+      get 'landlords/:id', to: 'landlords#show'
       post 'landlords/create'
-      get 'landlords/show', to: 'landlords#show'
       delete 'landlords/destroy'
     end
   end
-
-  # get '/properties', to: 'properties#index'
-  # get '/properties/:id', to: 'properties#show'
-
-  # get '/landlords', to: 'landlords#index'
-  # get '/landlords/:id', to: 'landlords#show'
-
 
   # catch all route
   get '/*path' => 'homepage#index'
