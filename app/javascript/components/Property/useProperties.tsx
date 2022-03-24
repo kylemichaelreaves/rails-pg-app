5a40960bb6ffd4ad7323f5d32b0dcc435205c20d
Kyle Reaves <89040357+KyleReaves@users.noreply.github.com>
2022-03-24 16:39:57 -0400

    Fixes to linting warnings…
    Calling the async function in the useInfiniteQuery hook seemed to do the
    trick…But I've also updated the types of the functions…

--- app/javascript/components/Property/useProperties.tsx
+++ app/javascript/components/Property/useProperties.tsx
@@ -2,14 +2,14 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { Property } from "./useProperty";
 
-export async function fetchProperties(pageParam: any) {
-  return await axios
-    .get("http://127.0.0.1:3000/api/v1/properties?page=" + pageParam)
-    .then((response) => response.data);
+export async function fetchProperties(pageParam: number | undefined): Promise<Property[]> {
+  return typeof pageParam === 'undefined'
+    ? Promise.reject(new Error('invalid pageParam'))
+    : await axios.get("http://127.0.0.1:3000/api/v1/properties?cursor=" + pageParam).then((response) => response.data);
 }
 
-export default function useProperties(page: number = 0) {
-  return useQuery<Property[], Error>(
+export default function useProperties(page?: number | undefined) {
+  return useQuery(
     ["properties", page],
     () => fetchProperties(page),
     { keepPreviousData: true, staleTime: 5000 }
