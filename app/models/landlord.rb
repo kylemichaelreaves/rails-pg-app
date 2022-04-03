446f2e1d4b12d6e60e58c1b5be737a5ba83e9b22
Kyle <kylereaves@gmail.com>
2022-04-03 10:12:08 -0400

    Landlord methods added
    Suddenly it occured to me to have model methods.

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,17 +1,27 @@
 class Landlord < ApplicationRecord
-    # no two Landlords can share the same Landlord.name
-    validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
 
-    has_and_belongs_to_many :properties
+  has_and_belongs_to_many :properties
 
-    def owns_multiple_properties?
-    end
+  def lives_outside_us?
+  end
 
-    def lives_outside_us?
-    end
+  def single_person?
+  end
 
-    def more_than_one_name?
-    end
+  def multiple_owners?
+    name.include? "&" and name.not.include? "LLC"
+  end
 
+  def llc?
+    name.include? "LLC"
+  end
 
+  def owns_multiple_properties?
+    number_of_properties_owned > 1
+  end
+
+  def number_of_properties_owned
+    Property.where(owner_name: name).count
+  end
 end
