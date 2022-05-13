064ee102b4e2a936d1b1fbcda9a6d31d2391594d
Kyle Reaves <89040357+KyleReaves@users.noreply.github.com>
2022-05-13 17:43:58 -0400

    React components updating with proper types

--- app/javascript/components/Address/Address.tsx
+++ app/javascript/components/Address/Address.tsx
@@ -2,3 +2,34 @@ import * as React from 'react'
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
+
+export interface AddressProps {
+    id: number;
+    streetAddress: string;
+    municipality: string;
+    state: string;
+    zipCode: string;
+    longitude?: number;
+    latitude?: number;
+    latitude_and_longitude?: string;
+    full_address?: string;
+  }
+
+export let blankAddress: AddressProps = {
+    id: 0,
+    streetAddress: "",
+    municipality: "",
+    state: "",
+    zipCode: ""
+  }
+
+export interface AddressesProps {
+    addresses: AddressProps[];
+}
+
+export default function Address() {
+    return (
+        <div>Address</div>
+    )
+}
+
