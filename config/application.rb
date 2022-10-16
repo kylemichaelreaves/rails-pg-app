b8f76d51634e1f38145336436b004cdc8241126f
Kyle <kylereaves@gmail.com>
2022-10-16 18:36:26 -0400

    Updating gitigore, pulling in Rails env variables
    from a hidden local_env.yml

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,16 @@ module RailsPgApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # load environment variables
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      if File.exists?(env_file)
+        YAML.load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
